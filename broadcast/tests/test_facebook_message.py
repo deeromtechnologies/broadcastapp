a71f8f8be6e72a1e5f787ab11c226f821ecd4f14
saheed cp <saheed887@gmail.com>
2017-01-19 21:52:43 +0530

    test passed after implementation of FaceBook function

--- broadcast/tests/test_facebook_message.py
+++ broadcast/tests/test_facebook_message.py
@@ -1,10 +1,7 @@
 import Facebook
-#def test_message_to_facebook():
- #   fb=Facebook()
-  #  assert fb.message_sent==True
- 
+
 def test_facebook_post():
     A = Facebook.FaceBook()
     message = 'hello world'
-    acknowledgement = A.facebook_post(message)
-    assert acknowledgement is True
+    respose = A.facebook_post(message)
+    assert response is True

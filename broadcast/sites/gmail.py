c38b720602d70a14344ea23d8eff863e0a9403e3
roshan <roshanmechoth@gmail.com>
2017-01-20 06:18:33 -0800

    modification in mail function

--- broadcast/sites/gmail.py
+++ broadcast/sites/gmail.py
@@ -3,27 +3,27 @@ class gmail:
     def __init__(self,username,pswd):
        self.username=username
        self.pswd=pswd
-    def authentication(self,server):
+    def authentication(self):
+       server=smtplib.SMTP('smtp.gmail.com',587)
        server.starttls()
-       server.login(self.username,self.pswd)
-       return "success"
-    def messagebro(self):
-       server.sendmail(self,msg)
+       try:
+          server.login(self.username,self.pswd)
+          return server
+       except smtplib.SMTPAuthenticationError:
+          return None
+
+    def messagebro(self,server,username,toaddress,msg):
+       server.sendmail(username,toaddress,msg)
        server.quit()
 
-def mail(message):
-   username="studio4g4@gmail.com"
-   pswd="9633206869"
-   txt=message
-   m=gmail(username,pswd)
-   server=smtplib.SMTP('smtp.gmail.com',587)       
-   try:
-      login=m.authentication(server)
-      if login=="success":
-         send= m.messagebro(txt)
-      else: 
-         send="fail"
-      return send
-   except:
-      return "authentication failed"
-
+def mail(msg,toaddress):
+   username="testmailcalicut@gmail.com"
+   pswd="asdasdqwe"
+   mail_obj=gmail(username,pswd)
+   server_obj=mail_obj.authentication()
+   if server_obj:
+      mail_obj.messagebro(server_obj,username,toaddress,msg)
+      return 'success'
+   else:
+      return 'fail'
+   

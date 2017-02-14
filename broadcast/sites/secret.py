e08cfa4c636647f22f0c3f3b3d70b7e1104527b3
roshan <roshanmechoth@gmail.com>
2017-02-13 21:54:13 -0800

    modification made on secret file

--- broadcast/sites/secret.py
+++ broadcast/sites/secret.py
@@ -1,5 +1,12 @@
 key= {
 'mail' :{'from' :'testmailcalicut@gmail.com',
-          'pswd':'asdasdqwe'
-         'toaddr':'roshanmechoth@gmail.com' }
-      }
+          'pswd':'asdasdqwe',
+         'toaddr':'roshanmechoth@gmail.com' },
+'twitter':{'consumer_key':'Sj846mujwyC9nwW84SgTsogoi',
+               'consumer_secret':'e0Oy0U5tlTe76ue4UV1INcFlwShMs4weNdIQbLPOgUqxerkdgA',
+               'access_token':'828853396832657409-uH307RIuHdgPiusUNxXmQgnf68SQ7s8',
+                'access_token_secret':'2Sr090gjsINUZKMq9331GAzxhEk6RaJPOniz3I3hsT9Ux'          }
+
+              }
+
+ 

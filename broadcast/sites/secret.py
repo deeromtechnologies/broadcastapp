cb0c4698dcde8e20923d31725d89f068694fb7b2
roshan <roshanmechoth@gmail.com>
2017-02-15 01:50:06 -0800

    facebook added

--- broadcast/sites/secret.py
+++ broadcast/sites/secret.py
@@ -5,8 +5,10 @@ key= {
 'twitter':{'consumer_key':'Sj846mujwyC9nwW84SgTsogoi',
                'consumer_secret':'e0Oy0U5tlTe76ue4UV1INcFlwShMs4weNdIQbLPOgUqxerkdgA',
                'access_token':'828853396832657409-uH307RIuHdgPiusUNxXmQgnf68SQ7s8',
-                'access_token_secret':'2Sr090gjsINUZKMq9331GAzxhEk6RaJPOniz3I3hsT9Ux'          }
-
+                'access_token_secret':'2Sr090gjsINUZKMq9331GAzxhEk6RaJPOniz3I3hsT9Ux'},          
+'facebook':{'access_token':'EAACEdEose0cBACfeZANohoqX8NsZCOCiE3d2wMKVscIcJvllrZASx5bZAhn5naZCeDUf3p7Fige1FsYoYYxx9nPIK5rrxNIqKIy4NlXzFaMxShPCD08Bc02WG9rlkCTu2iZBciWqriiPu7eBwzJVdZBTl9OGS03zTyzXTqALvOJfJnZBJJrums9YWNQnGV8aejgZD',
+'user_id':'712018825585122'
               }
+}
 
  
